2d6a4ef2329e24ec6b697bf6393eedba245a2ac5
Arees Somani <146130527+AreesSomani1234@users.noreply.github.com>
2026-06-26 20:57:21 -0400

    Remove Hot Stocks from top navigation

--- js/navigation.js
+++ js/navigation.js
@@ -3,7 +3,6 @@ window.CriusNavigation = {
     { label: "Home", href: "index.html", page: "home" },
     { label: "Search", href: "pages/search.html", page: "search" },
     { label: "Watchlist", href: "pages/watchlist.html", page: "watchlist" },
-    { label: "Hot Stocks", href: "pages/hot-stocks.html", page: "hot-stocks" },
     { label: "Markets", href: "pages/markets.html", page: "markets" },
     { label: "Articles", href: "pages/articles.html", page: "articles" },
     { label: "Profile", href: "pages/profile.html", page: "profile" }
